87cd542ba37d0a501856a0c5027853c0d894827c
Andrey <aftansin@mail.ru>
2025-08-21 09:59:28 +0300

    remove polling

--- dialogs/account.py
+++ dialogs/account.py
@@ -66,6 +66,7 @@ async def update_events(callback: CallbackQuery, button: Button, dialog_manager:
     pilot = dialog_manager.middleware_data.get('db_pilot')
     session = dialog_manager.middleware_data.get('session')
     await check_pilot_calendar(bot, session, pilot)
+    await dialog_manager.done()
 
 
 def account_info_window():
